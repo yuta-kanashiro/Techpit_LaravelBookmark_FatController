3b27e20b163878c5e85f0b920e0720582119c5ab
yutakanashiro <kanashiro713@gmail.com>
2022-06-03 20:58:45 +0900

    BookmarkController/createメソッドの処理をユースケースに移行（リファクタリング）

--- src/app/Http/Controllers/Bookmarks/BookmarkController.php
+++ src/app/Http/Controllers/Bookmarks/BookmarkController.php
@@ -23,6 +23,7 @@ use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 use App\Bookmark\UseCase\ShowBookmarkListPageUseCase;// 追加
 use App\Http\Requests\CreateBookmarkRequest; // 追加
+use App\Bookmark\UseCase\CreateBookmarkUseCase;// 追加
 
 class BookmarkController extends Controller
 {
@@ -129,27 +130,8 @@ class BookmarkController extends Controller
         // ログイン確認処理移動（web.php）
         // バリデーション移動（FormRequest）
 
-        // 下記のサービスでも同様のことが実現できる
-        // @see https://www.linkpreview.net/
-        $previewClient = new Client($request->url);
-        try {
-            $preview = $previewClient->getPreview('general')->toArray();
-
-            $model = new Bookmark();
-            $model->url = $request->url;
-            $model->category_id = $request->category;
-            $model->user_id = Auth::id();
-            $model->comment = $request->comment;
-            $model->page_title = $preview['title'];
-            $model->page_description = $preview['description'];
-            $model->page_thumbnail_url = $preview['cover'];
-            $model->save();
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-            throw ValidationException::withMessages([
-                'url' => 'URLが存在しない等の理由で読み込めませんでした。変更して再度投稿してください'
-            ]);
-        }
+        $useCase = new CreateBookmarkUseCase(); // 47行目のlistメソッドのような書き方もできる
+        $useCase->handle($request->url, $request->category, $request->comment);
 
         // 暫定的に成功時は一覧ページへ
         return redirect('/bookmarks', 302);
